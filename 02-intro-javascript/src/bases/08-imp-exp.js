062724c9022c0390aa473f4afced3d3fd3b06f85
jmgomez2018 <jmgametal@gmail.com>
2020-12-29 15:44:58 -0300

    22. Promesas

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,12 +1,13 @@
-import heroes, { owners } from '../data/heroes';
+// import heroes, { owners } from '../data/heroes';
+import heroes from '../data/heroes';
 
-console.log(owners);
+// console.log(owners);
 
-const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
+export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
-console.log( getHeroeById(2) );
+// console.log( getHeroeById(2) );
 
 // find?, filter
-const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
-console.log( getHeroesByOwner('Marvel') );
+// console.log( getHeroesByOwner('Marvel') );
