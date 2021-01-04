d6c5a457de37359e9bd213804db078c89e5fa31e
jmgomez2018 <jmgametal@gmail.com>
2021-01-04 14:28:02 -0300

    Seccion 05 - 6. Jest - Expect - toBe

--- 03-counter-app/src/tests/demo.test.js
+++ 03-counter-app/src/tests/demo.test.js
@@ -1,7 +1,16 @@
-test('should TRUE', () => {
-	const isActive = true;
+describe('Pruebas en el archivo demo.test.js', () => {
+	
+	test('Deben ser iguales los Strings', () => {
+		
+		// 1. Inicializacion
+		const mensaje = 'Hola Mundo';
+	
+		//2. Estimulo
+		const mensaje2 = `Hola Mundo`;
+	
+		// 3. Observar el comportamiento
+		expect(mensaje).toBe(mensaje2); // ===
+	});
 
-	if (isActive) {
-		throw new Error('No está activo!!!');
-	}
 });
+
