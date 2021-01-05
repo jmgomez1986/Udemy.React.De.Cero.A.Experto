57340699490e54cebf09670e3330cd30100c2e24
jmgomez2018 <jmgametal@gmail.com>
2021-01-05 17:55:57 -0300

    Seccion 05 - 17. Simular eventos - Click

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 import '@testing-library/jest-dom';
 import CounterApp from '../CounterApp';
 
 describe('Prueba en <CounterApp />', () => {
+	let wrapper = shallow(<CounterApp />);
+
+	// Esto se ejecuta con cada test
+	beforeEach(() => {
+		wrapper = shallow(<CounterApp />);
+	});
+
 	test('debe de mostrar <CounterApp /> correctamente ', () => {
-		const valorInicial = 10;
-		const wrapper = shallow(<CounterApp value={ valorInicial } />)
-
 		expect(wrapper).toMatchSnapshot();
-
 	});
 
 	test('debe de mostrar el valor por defecto de 100 ', () => {
 		const valorInicial = 100;
-		const wrapper = shallow( <CounterApp value={ 100 } />);
-		const textoParrafo = wrapper.find('h2').text();
+		const wrapper = shallow(<CounterApp value={100} />);
+		const counterText = wrapper.find('h2').text().trim();
 
-		expect(textoParrafo).toBe(`${valorInicial}`);
-
+		expect(counterText).toBe(`${valorInicial}`);
 	});
-	
+
+	test('debe incrementar contador con el boton +1 ', () => {
+		const btn1 = wrapper.find('button').at(0);
+
+		btn1.simulate('click');
+
+		const counterText = wrapper.find('h2').text().trim();
+
+		expect(counterText).toBe('11');
+	});
+
+	test('debe decrementar contador con el boton -1 ', () => {
+		const btn1 = wrapper.find('button').at(2);
+
+		btn1.simulate('click');
+
+		const counterText = wrapper.find('h2').text().trim();
+
+		expect(counterText).toBe('9');
+	});
 });
