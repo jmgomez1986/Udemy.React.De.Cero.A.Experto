00b3802886da6133cead2b2a750cfd20c941dfd1
jmgomez2018 <jmgametal@gmail.com>
2021-01-05 16:30:19 -0300

    Seccion 05 - 14. Enzyme - Testing unit

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+// import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import PrimeraApp from '../PrimeraApp';
 
 describe('Prueba en <PrimeraApp />', () => {
-	test('Debe mostrar el mensaje "Hola Matias" ', () => {
+	// test('Debe mostrar el mensaje "Hola Matias" ', () => {
+	// 	const saludo = 'Hola Matias';
+
+	// 	const {getByText} = render(<PrimeraApp saludo={saludo} />);
+
+	// 	expect(getByText(saludo)).toBeInTheDocument();
+
+	// });
+
+	test('debe de mostrar <PrimeraApp /> correctamente ', () => {
 		const saludo = 'Hola Matias';
+		const wrapper = shallow( <PrimeraApp saludo={saludo} /> );
 
-		const {getByText} = render(<PrimeraApp saludo={saludo} />);
-
-		expect(getByText(saludo)).toBeInTheDocument();
-
-	});
+		expect(wrapper).toMatchSnapshot();
+	});	
 	
 });
