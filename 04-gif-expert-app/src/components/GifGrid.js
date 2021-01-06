e9490b3a865aa0256423b8b9c81495baf6a50ae1
jmgomez2018 <jmgametal@gmail.com>
2021-01-06 15:29:51 -0300

    Seccion 06 - 12. className - Clases de css

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -26,19 +26,24 @@ export const GifGrid = ({category}) => {
 	}
 
 	return (
-		<div>
-			<h3>{category}</h3>
-			
-			{
-				images.map( (img) => 
-					<GifGridItem
-						key={img.id}
-						// img={img}
-						{...img}
-					/>
-				)
-			}
+		<>
 
-		</div>
+			<h3>{category}</h3>
+
+			<div className="card-grid">
+				
+				{
+					images.map( (img) => 
+						<GifGridItem
+							key={img.id}
+							// img={img}
+							{...img}
+						/>
+					)
+				}
+
+			</div>
+			
+		</>
 	)
 }
