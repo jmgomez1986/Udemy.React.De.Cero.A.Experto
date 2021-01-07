e84dd41ff485e0a459084f69683c44282e4bbf55
jmgomez2018 <jmgametal@gmail.com>
2021-01-07 11:30:03 -0300

    Seccion 06 - 16. Animaciones por CSS en nuestra aplicación

--- 04-gif-expert-app/src/hooks/useFetchGifs.js
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,22 +1,18 @@
 import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
-export const useFetchGifs = ({ category }) => {
+export const useFetchGifs = (category) => {
 	const [state, setState] = useState({
 		data: [],
 		loading: true,
 	});
 
 	useEffect(() => {
-		getGifs(category).then((imgs) => {
-			setTimeout(() => {
-				
-				setState({
-					data: imgs,
-					loading: false,
-				})
-				
-			}, 3000);
+		getGifs(category).then((imgs) => {				
+			setState({
+				data: imgs,
+				loading: false,
+			});
 		}
 		);
 	}, [category]);
