ce6a1f7573d5cb1bced334338e8b41e136aa1721
jmgomez2018 <jmgametal@gmail.com>
2021-01-07 18:01:26 -0300

    Seccion 08 - 8. Simular cambios en un input

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -4,11 +4,17 @@ import { AddCategory } from '../../components/AddCategory';
 
 describe('Pruevas en el componente <AddCategory />', () => {
 	const setCategories = () => {}
+	const wrapper = shallow(<AddCategory setCategorias={setCategories} />);
 
 	test('debe de mostrarse el componente <AddCategoy /> correctamente ', () => {
-		const wrapper = shallow(<AddCategory setCategorias={setCategories} />);
-
 		expect(wrapper).toMatchSnapshot();
 	});
+
+	test('debe de cambiar la caja de texto ', () => {
+		const input = wrapper.find('input');
+		const value = 'Hola Mundo!!!';
+
+		input.simulate('change', {target: {value}});
+	});	
 	
 });
