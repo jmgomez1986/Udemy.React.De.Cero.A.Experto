c07f33bdbf65d3b80035bcf93d420a0ab6c24dcd
jmgomez2018 <jmgametal@gmail.com>
2021-01-07 15:41:55 -0300

    Seccion 08 - 5. Pruebas del componente - GifGridItem

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -2,18 +2,42 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
-describe('Prueba en componente <GifGridItem />', () => {
+describe('Prueba en componente <GifGridItem />', () => {	
 	const title = 'Un titulo';
 	const url = 'https://localhost/algo.jpg';
+	const wrapper = shallow(
+		<GifGridItem
+			title={title}
+			url={url}
+		/>);
 
 	test('debe de mostrar el componente correctamente ', () => {
-		const wrapper = shallow(
-			<GifGridItem
-				title={title}
-				url={url}
-			/>);
-
 			expect(wrapper).toMatchSnapshot();
 	});
+
+	test('debe de tener un parrafo con el titulo ', () => {
+		const gifGridItemTitle = wrapper.find('p').text().trim();
+
+		expect(gifGridItemTitle).toBe(title);
+	});
+	
+	test('debe de tener una imagen con la url y alt igual a los props ', () => {
+		const gifGridItemImg = wrapper.find('img');
+
+		expect(gifGridItemImg.prop('src')).toBe(url);
+		expect(gifGridItemImg.prop('alt')).toBe(title);
+
+		// const gifGridItemImg = wrapper.find('img').props();
+
+		// expect(gifGridItemImg.src).toBe(url);
+		// expect(gifGridItemImg.alt).toBe(title);
+	});
+
+	test('debe tener la clase animate__fadeIn ', () => {
+		const div = wrapper.find('div');
+		const className = div.props().className;
+
+		expect(className.includes('animate__fadeIn')).toBe(true);
+	});	
 	
 });
