87d0446eb97768b60c4c085402b14e786da2348f
jmgomez2018 <jmgametal@gmail.com>
2021-01-07 10:57:27 -0300

    Seccion 06 - 15. useFetchGifs - obtener imágenes y bandera de carga

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,43 +1,30 @@
-// import React, { useEffect, useState } from 'react'
-// import { getGifs } from '../helpers/getGifs';
-// import { GifGridItem } from './GifGridItem';
 import React from 'react';
+import { GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
 
-	// const [images, setImages] = useState([]);
-
-	// useEffect(() => {
-	// 	getGifs(category)
-	// 		.then( setImages );
-	// }, [category]);
-
-	const {data, loading} = useFetchGifs();
-
-	console.log(data);
-	console.log(loading);
+	const {data: images, loading} = useFetchGifs(category);
 
 	return (
 		<>
 
 			<h3>{category}</h3>
 
-			{loading ? 'Cargando...' : 'Data cargada'}
+			{loading && <p>Loading...</p>}
 
-			{/* <div className="card-grid">
+			<div className="card-grid">
 				
 				{
 					images.map( (img) => 
 						<GifGridItem
 							key={img.id}
-							// img={img}
 							{...img}
 						/>
 					)
 				}
 
-			</div> */}
+			</div>
 
 		</>
 	)
--- 04-gif-expert-app/src/hooks/useFetchGifs.js
+++ 04-gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
 
-export const useFetchGifs = () => {
-	const [state, setState] = useState(
-		{
-			data: [],
-			loading: true
+export const useFetchGifs = ({ category }) => {
+	const [state, setState] = useState({
+		data: [],
+		loading: true,
+	});
+
+	useEffect(() => {
+		getGifs(category).then((imgs) => {
+			setTimeout(() => {
+				
+				setState({
+					data: imgs,
+					loading: false,
+				})
+				
+			}, 3000);
 		}
-	);
-
-	setTimeout(() => {
-		setState(
-			{
-				data: [1, 2, 3, 4],
-				loading: false
-			}
-		)
-	}, 3000);
+		);
+	}, [category]);
 
 	return state;
 };
