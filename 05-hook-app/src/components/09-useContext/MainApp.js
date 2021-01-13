05c32bc64dcf50f92945ef981b4671f06476bb73
jmgomez2018 <jmgametal@gmail.com>
2021-01-13 17:23:01 -0300

    Seccion 11 - 7. CreateContext y useContext

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import { AppRouter } from './AppRouter';
+import { UserContext } from './UserContext';
 
 export const MainApp = () => {
-	return <AppRouter />;
+
+	const user = {
+		id: 1234,
+		name: 'Matias',
+		email: 'jmgametal@gmail.com'
+	}
+
+	return (
+		<UserContext.Provider value={user}>
+			<AppRouter />
+		</UserContext.Provider>
+	);
 };
