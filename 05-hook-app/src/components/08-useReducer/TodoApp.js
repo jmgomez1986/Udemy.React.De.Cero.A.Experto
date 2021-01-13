535d4ade50c05d6902c6ef7dc0b6eed06170a3d4
jmgomez2018 <jmgametal@gmail.com>
2021-01-13 10:51:38 -0300

    Seccion 10 - 7. Agregar un nuevo TODO

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -12,9 +12,27 @@ const initialState = [
 ];
 
 export const TodoApp = () => {
-	const [todos] = useReducer(todoReducer, initialState);
+	const [todos, dispatch] = useReducer(todoReducer, initialState);
 
 	console.log(todos);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const newTodo = {
+			id: new Date().getTime(),
+			desc: 'Nueva Tarea',
+			done: false,
+		};
+
+		const action = {
+			type: 'add',
+			payload: newTodo,
+		};
+
+		dispatch(action);
+
+	};
 
 	return (
 		<div>
@@ -22,51 +40,43 @@ export const TodoApp = () => {
 			<hr />
 
 			<div className='row'>
-
 				<div className='col-7'>
-					
 					<ul className='list-grouo list-group-flush'>
-						{
-							todos.map((todo, i) => {
-								return (
-									<li
-										key={todo.id}
-										className='list-group-item'>
-										<p className='text-center'>{i + 1}. {todo.desc}</p>
-										<button className='btn btn-danger'>Borrar</button>
-									</li>
-								);
-							})
-						}
-				</ul>
-					
+						{todos.map((todo, i) => {
+							return (
+								<li key={todo.id} className='list-group-item'>
+									<p className='text-center'>
+										{i + 1}. {todo.desc}
+									</p>
+									<button className='btn btn-danger'>Borrar</button>
+								</li>
+							);
+						})}
+					</ul>
 				</div>
 
 				<div className='col-5'>
-					
 					<h4>Agregar TODO</h4>
 					<hr />
 
-					<form>
-
-						<input 
-							type="text"
-							name="description"
-							className="form-control"
-							placeholder="Aprender ..."
-							autoComplete="off" />
+					<form onSubmit={handleSubmit}>
+						<input
+							type='text'
+							name='description'
+							className='form-control'
+							placeholder='Aprender ...'
+							autoComplete='off'
+						/>
 
 						<button
-							className="btn btn-outline-primary mt-1 btn-block" >
+							className='btn btn-outline-primary mt-1 btn-block'
+							type='submit'
+						>
 							Agregar
 						</button>
-
 					</form>
-								
 				</div>
-
 			</div>
-
 		</div>
 	);
 };
