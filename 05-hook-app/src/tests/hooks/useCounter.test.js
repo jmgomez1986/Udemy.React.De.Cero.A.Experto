1cf826fa115601b181099f3c55cf2a266768ab13
jmgomez2018 <jmgametal@gmail.com>
2021-01-14 11:24:22 -0300

    Seccion 12 - 5. Ejecutar funciones del customHook dentro de las pruebas

--- 05-hook-app/src/tests/hooks/useCounter.test.js
+++ 05-hook-app/src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useCounter } from '../../hooks/useCounter';
 
 describe('Prueba en el custom Hook useCounter', () => {
@@ -9,7 +9,45 @@ describe('Prueba en el custom Hook useCounter', () => {
 		expect(typeof result.current.increment).toBe('function');
 		expect(typeof result.current.decrement).toBe('function');
 		expect(typeof result.current.reset).toBe('function');
-
 	});
-	
+
+	test('debe de tener el counter en 100 ', () => {
+		const { result } = renderHook(() => useCounter(100));
+
+		expect(result.current.counter).toBe(100);
+	});
+
+	test('debe de incrementar el counter en 1 ', () => {
+		const { result } = renderHook(() => useCounter());
+		const { increment } = result.current;
+
+		act(() => {
+			increment();
+		});
+
+		expect(result.current.counter).toBe(11);
+	});
+
+	test('debe de decrementar el counter en 1 ', () => {
+		const { result } = renderHook(() => useCounter());
+		const { decrement } = result.current;
+
+		act(() => {
+			decrement();
+		});
+
+		expect(result.current.counter).toBe(9);
+	});
+
+	test('debe de resetear el counter al valor inicial ', () => {
+		const { result } = renderHook(() => useCounter());
+		const { increment, reset } = result.current;
+
+		act(() => {
+			increment();
+			reset();
+		});
+
+		expect(result.current.counter).toBe(10);
+	});
 });
