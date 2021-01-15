0383e3d5d340d297dd8219d8f1bdd6b32ffdc2ba
jmgomez2018 <jmgametal@gmail.com>
2021-01-15 10:28:46 -0300

    Seccion 12 - 11. Pruebas restantes de mi Reducer

--- 05-hook-app/src/tests/components/08useReducer/todoReducer.test.js
+++ 05-hook-app/src/tests/components/08useReducer/todoReducer.test.js
@@ -1,14 +1,14 @@
 import { todoReducer } from '../../../components/08-useReducer/todoReducer';
 import { demoTodos } from '../../fixtures/demoTodos';
 
-describe('Pruevas en todoReducer', () => {
+describe('Pruebas en todoReducer', () => {
 	test('debe de retornar el estado por defecto ', () => {
 		const state = todoReducer(demoTodos, {});
 
 		expect(state).toEqual(demoTodos);
 	});
 
-	test('debe de agregar un nuevo todo ', () => {
+	test('debe de agregar un nuevo TODO ', () => {
 		const newTodo = {
 			id: 3,
 			desc: 'Aprender Go',
@@ -25,4 +25,31 @@ describe('Pruevas en todoReducer', () => {
 		expect(state.length).toEqual(3);
 		expect(state).toEqual([...demoTodos, newTodo]);
 	});
+
+	test('debe de eliminar un TODO ', () => {
+
+		const action = {
+			type: 'delete',
+			payload: 1
+		};
+
+		const state = todoReducer(demoTodos, action);
+
+		expect(state.length).toBe(1);
+		expect(state).toEqual([demoTodos[1]]);
+	});
+
+	test('debe de hacer el Toggle de un TODO ', () => {
+
+		const action = {
+			type: 'toggle',
+			payload: 1
+		};
+
+		const state = todoReducer(demoTodos, action);
+
+		expect(state[0].done).toEqual(true);
+		expect(state[1]).toEqual(demoTodos[1]);
+	});
+
 });
