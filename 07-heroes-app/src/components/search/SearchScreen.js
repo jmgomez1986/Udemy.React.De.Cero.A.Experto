42e96d73c0790aed2bfc26b0fba78f570c60ba20
jmgomez2018 <jmgametal@gmail.com>
2021-01-18 15:25:51 -0300

    Seccion 14 - 15. Aplicar filtro de Héroes - QueryString

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import { heroes } from '../../data/heroes';
 import { useForm } from '../../hooks/useForm';
 import { HeroCard } from '../heroes/HeroCard';
 
-export const SearchScreen = () => {
-	const heroesFiltered = heroes;
-
+export const SearchScreen = ({ history }) => {
+	const location = useLocation();
+	const { q: query = '' } = queryString.parse(location.search);
 	const [{ searchText }, handleInputChange, reset] = useForm({
-		searchText: '',
+		searchText: query,
 	});
 
+	const heroesFiltered = heroes;
+	
 	const handleSearch = (e) => {
 		e.preventDefault();
 
-		console.log(searchText);
+		history.push(`?q=${searchText}`);
 
 		reset();
 	};
