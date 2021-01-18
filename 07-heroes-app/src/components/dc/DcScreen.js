d9840bde5d4013a9965a3064e16b64f4c682eeba
jmgomez2018 <jmgametal@gmail.com>
2021-01-18 11:34:30 -0300

    Seccion 14 - 8. Lista de Heroes

--- 07-heroes-app/src/components/dc/DcScreen.js
+++ 07-heroes-app/src/components/dc/DcScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { HeroList } from '../heroes/HeroList'
 
 export const DcScreen = () => {
 	return (
 		<div>
 			<h1>Dc Screen</h1>
+
+			<HeroList publisher='DC Comics' />
+
 		</div>
 	)
 }
---
+++ 07-heroes-app/src/selectors/getHeroesByPublisher.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { heroes } from '../data/heroes';
+
+export const getHeroesByPublisher = (publisher) => {
+	const validPublisher = ['DC Comics', 'Marvel Comics'];
+
+	if (!validPublisher.includes(publisher)) {
+		throw new Error(`Publisher "${publisher}" no es correcto`);
+	}
+
+	return heroes.filter((hero) => hero.publisher === publisher);
+};
