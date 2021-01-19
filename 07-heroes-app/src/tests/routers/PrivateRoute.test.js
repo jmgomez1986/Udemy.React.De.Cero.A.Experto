3d5f3bb2cdf83c9f08d027c501bcdcaf1ad1dc63
jmgomez2018 <jmgametal@gmail.com>
2021-01-19 16:56:08 -0300

    Seccion 16 - 6. Probar que el localStorage sea llamado con argumentos

--- 07-heroes-app/src/tests/routers/PrivateRoute.test.js
+++ 07-heroes-app/src/tests/routers/PrivateRoute.test.js
@@ -7,8 +7,11 @@ describe('Pruebas en PrivateRoute', () => {
 	const props = {
 		location: {
 			pathname: '/marvel',
+			search: ''
 		},
 	};
+
+	Storage.prototype.setItem = jest.fn();
 
 	test('debe de mostrar el componente si esta autenticado y guardar en localStorage ', () => {
 		const wrapper = mount(
@@ -22,5 +25,6 @@ describe('Pruebas en PrivateRoute', () => {
 		);
 
 		expect(wrapper.find('span').exists()).toBe(true);
+		expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/marvel');
 	});
 });
