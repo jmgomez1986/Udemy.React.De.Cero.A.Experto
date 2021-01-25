c21ba6ea564788e82d9197fb19a71f609220618f
jmgomez2018 <jmgametal@gmail.com>
2021-01-25 12:59:08 -0300

    Seccion 20 - 10. Mostrar las notas en el menú lateral

--- 08-journal-app/src/components/journal/JournalEntry.js
+++ 08-journal-app/src/components/journal/JournalEntry.js
@@ -1,27 +1,47 @@
 import React from 'react';
+import dayjs from "dayjs";
+import moment from 'moment';
 
-export const JournalEntry = () => {
+// advancedFormat permite usar la fecha ordinal entre otras opciones
+const advancedFormat = require("dayjs/plugin/advancedFormat");
+dayjs.extend(advancedFormat);
+
+export const JournalEntry = ({id, date, title, body, url}) => {
+
+	const noteDate = dayjs(date);
+
+	// const noteDate = moment(date);
+
 	return (
 		<div className='journal__entry pointer'>
-			<div
-				className='journal__entry-picture'
-				style={{
-					backgroundSize: 'cover',
-					backgroundImage:
-						'url(https://earthsky.org/upl/2018/12/comet-wirtanen-Jack-Fusco-dec-2018-Anza-Borrego-desert-CA-e1544613895713.jpg)',
-				}}
-			></div>
+
+			{
+
+				url &&
+
+				<div
+					className='journal__entry-picture'
+					style={{
+						backgroundSize: 'cover',
+						backgroundImage:
+							`url(${url})`,
+					}}
+				></div>
+
+			}
 
 			<div className='journal__entry-body'>
-				<p className='journal__entry-title'>Un nuevo día</p>
+				<p className='journal__entry-title'>{title}</p>
 				<p className='journal__entry-content'>
-					Reprehenderit id in duis consectetur deserunt veniam fugiat.
+					{body}
 				</p>
 			</div>
 
 			<div className='journal__entry-date-box'>
-				<span>Monday</span>
-				<h4>28</h4>
+				{/* <span>{noteDate.format('dddd')}</span> */}
+				{/* <h4>{noteDate.format('Do')}</h4> */}
+				<span>{noteDate.format('dddd')}</span>
+        <h4>{noteDate.format("Do")}</h4>
 			</div>
 		</div>
 	);
