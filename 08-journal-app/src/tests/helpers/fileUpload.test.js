e9e43f91266c06adc094451440e8df2fc8c8835f
jmgomez2018 <jmgametal@gmail.com>
2021-01-27 14:54:29 -0300

    Seccion 21 - 5. Cloudinary SDK - Delete image

--- 08-journal-app/src/tests/helpers/fileUpload.test.js
+++ 08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,15 +1,26 @@
-const { fileUpload } = require('../../helpers/fileUpload');
+import cloudinary from 'cloudinary';
+import { fileUpload } from '../../helpers/fileUpload';
+
+cloudinary.config({ 
+  cloud_name: 'dognzyxa8', 
+  api_key: '678912487314479', 
+  api_secret: 'iFYs2Qzv-dqCx2t-PmWTfZ1WS3A' 
+});
 
 describe('Pruebas en fileUpload', () => {
 	test('debe de cargar un archivo y retornar el URL ', async () => {
 		const resp = await fetch('https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png');
 		const blob = await resp.blob();
-
 		const file = new File([blob], 'foto.jpg');
-
 		const url = await fileUpload(file);
 
 		expect(typeof url).toBe('string');
+
+		// Borrar imagen por ID
+		const segments = url.split('/');
+		const imgId = segments[segments.length - 1].replace('.png', '');
+
+		await cloudinary.v2.api.delete_resources(imgId);
 	});
 
 	test('debe de retornar un erros ', async () => {
